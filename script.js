ce61b2afc6bbb55e134cf8a8b9564aabdfc0e7da
Devanshu Chandu Mankar <158685271+devanshumankar@users.noreply.github.com>
2025-11-27 16:51:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your code here
+let dragElement=null
+document.addEventListener("dragstart",(e)=>{
+	dragElement=e.target
+})
+document.addEventListener("dragover",(e)=>{
+	e.preventDefault();
+})
+document.addEventListener("drop",(e)=>{
+	e.preventDefault();
+	let target=e.target;
+
+	if(target!=dragElement){
+			let temp=document.createElement("div");
+
+			target.replaceWith(temp);
+			dragElement.replaceWith(target);
+			temp.replaceWith(dragElement);
+	}
+})
+
